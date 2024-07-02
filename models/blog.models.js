113687c0a1df850b8bc5c0160e5271c3e48683ad
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2024-07-02 20:14:35 +0530

    Blog and subscription models added

---
+++ models/blog.models.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+    {
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        subtitle: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true,
+            minLength: 50
+        },
+        readerCount: {
+            type: Number,
+            default: 0
+        },
+        shareCount: {
+            type: Number,
+            default: 0
+        }
+    },
+    {timestamps: true}
+)
+
+export const Blog = mongoose.model("Blog", blogSchema)
---
+++ models/subscription.models.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from 'mongoose';
+
+const subscriptionSchema = new mongoose.Schema(
+    {
+        blogger: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        subscriber: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },{timestamps: true}
+)
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
